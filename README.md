a725c2bc3143fc9a7913c80f9fa56a35395dd4e3
javm <quetzal1910@gmail.com>
2016-11-17 09:18:49 -0500

    Adding some more instructions

--- README.md
+++ README.md
@@ -3,34 +3,61 @@ exif-extractor
 
 SUMMARY
 ========
+It is an API to be integrated with a UI.
+
+TODO:
+1. Search engine
+2. User interface methods for the search
+
 Jose A. Villarreal (c) 2016.
 
 
 Dependencies
 ============
-- Express
-- Node.js
-- Mongodb
-See package.js
+- Express 
+- Node.js v6.3.0 (npm v3.10.3) >=
+- Mongodb v2.4.12 >=
+- See package.js
 
 Usage
 ===========
 -  Start:
+```
 node api.js
 node api.js --reset
 will reset the mongodb table that contains the metadata.
-
+```
 - The database is exif-extractor
 - The table is exif
 - Port 8010
 - exif table structure:
+```
 {
-	id: {imageKey},
-	exif: {exifDataExtracted}
+	key: 'imageKey',
+	metadata: {exifDataExtractedObject}
 }
-- Query endpoint
-GET localhost:8010/exif
+```
+- Query endpoints
+1. GET localhost:8010/exif
 gets the list of all exif photos metadata stored
+```
+{
+ items: [object],
+ count: {number}
+}
+```
+2. GET localhost:8010/exif/:key
+
+GET http://localhost:8010/exif/001f4fda-11a9-439d-b579-813d7624afe7.ecd9c0eb-65ec-4e30-b537-a3690f81616b
+gets the specific item with key
+
+```
+{
+ key: 001f4fda-11a9-439d-b579-813d7624afe7.ecd9c0eb-65ec-4e30-b537-a3690f81616b,
+metadata: {exifDataExtractedObject}
+}
+```
+
 
 
 
--- api.js
+++ api.js
@@ -2,13 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var app = express();
-app.use(bodyParser.urlencoded({extended: true}));               // Get POST Data.
+app.use(bodyParser.urlencoded({extended: true})); // Get POST Data.
 app.use(bodyParser.json());
 var config = require('./config');
 config.set('baseDir', process.cwd());
 
-//Initializing...
 
+//Initializing...
 var Extractor = require('./models/extractor');
 var Exif = require('./models/exif');
 
@@ -31,17 +31,42 @@ function connectCb(err){
 
 }
 
+// Use bluebird
+mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost/exif-extractor', connectCb);
 
+//Just saying hello
 app.get('/', (req, res, next) => {
   res.json({msg: 'EXIF Extractor!'});
 });
 
+//Get all the exif metadata saved in the database in JSON format in the items array and the total number if items 'count'
 app.get('/exif', (req, res, next) => {
 
   Exif.find().then(all => {
-    res.json(all);
+    res.json({items: all, count: all.length});
+  }).catch(err => {
+    res.status(500);
+    return res.json({ error: err });
   });
-}); 
+});
 
-app.listen(config.get('port'));
+
+//Get a specific photo exif metadata by key, the name of the image file (without '.jpg')
+app.get('/exif/:key', (req, res, next) => {
+  let key = req.params.key;
+  Exif.findOne({key: key}).then(item => {
+    if(!item){
+      res.status(404);
+      return res.json({ error: `No item found with key ${key}` });
+    }
+    res.json({item: item});
+  }).catch(err => {
+    res.status(500);
+    return res.json({ error: err });
+  });
+});
+
+let port = config.get('port');
+app.listen(port);
+console.info(`App listening on port ${port}`);
